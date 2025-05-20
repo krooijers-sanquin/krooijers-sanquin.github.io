6cf54a39373e1a1aca3687ed5ca087af366e48d8
Koos Rooijers <k.rooijers@sanquin.nl>
2025-05-20 14:47:19 +0200

    Astro build output

---
+++ _astro/you-were-building-a-dag-anyway.DSckP0hl.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["_astro/you-were-building-a-dag-anyway.BnaVxYCK.js","_astro/utils.AjeXWoOT.js","_astro/index.Czo94tge.js","_astro/astro/assets-service.DeftQf-f.js"])))=>i.map(i=>d[i]);
+import{_ as t}from"./astro/assets-service.DeftQf-f.js";async function o(){return t(()=>import("./you-were-building-a-dag-anyway.BnaVxYCK.js"),__vite__mapDeps([0,1,2,3]))}const e="@@ASTRO-LINKS@@",c="@@ASTRO-STYLES@@",r="@@ASTRO-SCRIPTS@@",l={__astroPropagation:!0,getMod:o,collectedLinks:e,collectedStyles:c,collectedScripts:r};export{l as default};
